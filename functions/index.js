5f2bb2cff5ddb970830a892bc1def0f0d1702814
rollinsafary <Karlik930Defacer(#)>
2018-04-08 15:55:38 +0300

    minor changes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const admin = require('firebase-admin')
 const serviceAccount = require('./findMiniAccountKey.json')
 const functions = require('firebase-functions')
 const cors = require('cors')({ origin: true })
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://find-mini.firebaseio.com'
