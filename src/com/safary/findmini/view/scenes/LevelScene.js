dd08a88a15518d56471328f0b24096aee299801b
rollinsafary <Karlik930Defacer(#)>
2018-04-14 22:24:47 +0300

    minor changes

--- src/com/safary/findmini/view/scenes/LevelScene.js
+++ src/com/safary/findmini/view/scenes/LevelScene.js
@@ -23,6 +23,7 @@ export default class LevelScene extends FindMiniScene {
     this.levelNavigation = new LevelNavigationView(this)
     this.navigationContainer.add(this.levelNavigation)
     this.levelNavigation.setSoundState(soundState)
+    this.navigationContainer.depth = 10
   }
 
   createLevels (level, maxLevels) {
@@ -41,21 +42,22 @@ export default class LevelScene extends FindMiniScene {
         this.createDisabledLevel(i)
       }
     }
-    console.warn(this)
   }
 
   createOpenedLevel (level) {
     const position = this.generatePosition()
     const levelButton = createButton(this, position.x, position.y, 'level', level, this.emitLevelClick, this, level)
     this.levelsContainer.add(levelButton)
-    this.add.text(levelButton.x, levelButton.y, level).setOrigin(0.5)
+    const text = this.add.text(levelButton.x, levelButton.y, level).setOrigin(0.5)
+    this.levelsContainer.add(text)
   }
 
   createDisabledLevel (level) {
     const position = this.generatePosition()
     const levelButton = createButton(this, position.x, position.y, 'levelDisabled', level)
     this.levelsContainer.add(levelButton)
-    this.add.text(levelButton.x, levelButton.y, level).setOrigin(0.5)
+    const text = this.add.text(levelButton.x, levelButton.y, level).setOrigin(0.5)
+    this.levelsContainer.add(text)
   }
   generatePosition () {
     const position = this.lastLevelPosition
@@ -74,7 +76,7 @@ export default class LevelScene extends FindMiniScene {
 
   get lastLevelPosition () {
     const levels = this.levelsContainer.list
-    const lastLevel = levels[levels.length - 1]
+    const lastLevel = levels[levels.length - 2]
     const position = lastLevel ? lastLevel.list[0] : {x: -30, y: 150}
     return {x: position.x, y: position.y}
   }
