3a706e20943610fd38f6f77497748293c3f7b367
rollinsafary <Karlik930Defacer(#)>
2018-05-08 19:26:00 +0300

    GameConfig fix

--- src/com/safary/findmini/constants/GameConfig.js
+++ src/com/safary/findmini/constants/GameConfig.js
@@ -7,23 +7,4 @@ export const gameConfig = {
   height: 960,
   backgroundColor: '#626262',
   parent: 'gameContainer',
-  physics: {
-    default: 'matter',
-    matter: {
-      setBounds: {
-        x: 0,
-        y: -200,
-        width: 540,
-        height: 960 + 200,
-        thickness: 64,
-      },
-      debug: false,
-      enableSleeping: true,
-      debugBodyColor: 0x00ffff,
-      gravity: {
-        x: 0,
-        y: 2.5,
-      },
-    },
-  },
 }
